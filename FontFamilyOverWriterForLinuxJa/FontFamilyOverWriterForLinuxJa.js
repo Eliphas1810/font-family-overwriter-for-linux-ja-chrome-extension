67ec5372d5b73f813bce4db0f44669f253d39c93
Eliphas1810 <152246411+Eliphas1810@users.noreply.github.com>
2024-01-16 22:42:49 +0900

    add FontFamilyOverWriterForLinuxJa.js

---
+++ FontFamilyOverWriterForLinuxJa/FontFamilyOverWriterForLinuxJa.js
@@ -0,0 +1,27 @@
+window.onload = function () {
+
+    var fontFamilyForLinuxJa = "'TakaoPGothic', 'Noto Sans CJK JP', 'YuGothic', 'Hiragino Kaku Gothic ProN', 'Hiragino Sans', 'Yu Gothic', 'Meiryo', 'MS PGothic', sans-serif";
+
+    document.getElementsByTagName("html".toUpperCase())[0].style.fontFamily = fontFamilyForLinuxJa;
+    document.getElementsByTagName("body".toUpperCase())[0].style.fontFamily = fontFamilyForLinuxJa;
+
+    var divTags = document.getElementsByTagName("div".toUpperCase());
+    for (var index = 0; index < divTags.length; index++) {
+        divTags[index].style.fontFamily = fontFamilyForLinuxJa;
+    }
+
+    var pTags = document.getElementsByTagName("p".toUpperCase());
+    for (var index = 0; index < pTags.length; index++) {
+        pTags[index].style.fontFamily = fontFamilyForLinuxJa;
+    }
+
+    var inputTags = document.getElementsByTagName("input".toUpperCase());
+    for (var index = 0; index < inputTags.length; index++) {
+        inputTags[index].style.fontFamily = fontFamilyForLinuxJa;
+    }
+
+    var textareaTags = document.getElementsByTagName("textarea".toUpperCase());
+    for (var index = 0; index < textareaTags.length; index++) {
+        textareaTags[index].style.fontFamily = fontFamilyForLinuxJa;
+    }
+};
