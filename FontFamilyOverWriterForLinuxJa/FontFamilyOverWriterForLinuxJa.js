729a11f8a2cc10d4277ca5a39284aa55e8e6fec7
Eliphas1810 <152246411+Eliphas1810@users.noreply.github.com>
2024-01-22 01:43:21 +0900

    CSS important keyword and Array.forEach()

--- FontFamilyOverWriterForLinuxJa/FontFamilyOverWriterForLinuxJa.js
+++ FontFamilyOverWriterForLinuxJa/FontFamilyOverWriterForLinuxJa.js
@@ -1,24 +1,12 @@
 var fontFamilyForLinuxJa = "'TakaoPGothic', 'Noto Sans CJK JP', 'YuGothic', 'Hiragino Kaku Gothic ProN', 'Hiragino Sans', 'Yu Gothic', 'Meiryo', 'MS PGothic', sans-serif";
 
-document.getElementsByTagName("html".toUpperCase())[0].style.fontFamily = fontFamilyForLinuxJa;
-document.getElementsByTagName("body".toUpperCase())[0].style.fontFamily = fontFamilyForLinuxJa;
+["html", "body"].forEach(function (tagName) {
+    document.getElementsByTagName(tagName.toUpperCase())[0].style.setProperty("font-family", fontFamilyForLinuxJa, "important");
+});
 
-var divTags = document.getElementsByTagName("div".toUpperCase());
-for (var index = 0; index < divTags.length; index++) {
-    divTags[index].style.fontFamily = fontFamilyForLinuxJa;
-}
-
-var pTags = document.getElementsByTagName("p".toUpperCase());
-for (var index = 0; index < pTags.length; index++) {
-    pTags[index].style.fontFamily = fontFamilyForLinuxJa;
-}
-
-var inputTags = document.getElementsByTagName("input".toUpperCase());
-for (var index = 0; index < inputTags.length; index++) {
-    inputTags[index].style.fontFamily = fontFamilyForLinuxJa;
-}
-
-var textareaTags = document.getElementsByTagName("textarea".toUpperCase());
-for (var index = 0; index < textareaTags.length; index++) {
-    textareaTags[index].style.fontFamily = fontFamilyForLinuxJa;
-}
+["div", "p", "input", "textarea"].forEach(function (tagName) {
+    var tags = document.getElementsByTagName(tagName.toUpperCase());
+    for (var index = 0; index < tags.length; index++) {
+        tags[index].style.setProperty("font-family", fontFamilyForLinuxJa, "important");
+    }
+});
